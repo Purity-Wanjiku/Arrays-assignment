5956a61166328bde9d87950a4efcaf396b78a786
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-03-13 22:12:49 +0300

    Done arrays assignment

---
+++ arrays.js
@@ -0,0 +1,24 @@
+arr1 = [3,7,34,90,12];
+const result = (arr1.slice(-1));
+console.log("last element",result);
+arr2 = [true,"green","where",12,56];
+const result1 = (arr2.slice(-1));
+console.log("last element1",result1);
+
+myPets = ["Cow","Bird","Snake","Dog"];
+console.log(myPets.join());
+
+var arr3 = [-5,9,5,3,2,-3,6,8,4,1];
+let sortit = arr3.sort()
+console.log("sort",sortit);
+
+// var arr = ["apple","mango","apple","orange","mango","mango"];
+// let duplicate = arr.filter()
+
+const arr5 = ["the","way","x",4];
+let result2 = arr5.includes("the");  //we use includes() to know if a value exists in an array
+console.log("find",result2);
+
+let word = "sevink";
+let sorted = word.sort();
+console.log(sorted);
